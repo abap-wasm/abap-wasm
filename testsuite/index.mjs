fbc301f9a3009b057b8d33af15aeb3797c968882
Lars Hvam <larshp@hotmail.com>
2024-01-03 16:50:15 +0100

    add converted testsuites

--- testsuite/index.mjs
+++ testsuite/index.mjs
@@ -1,4 +1,5 @@
 import * as fs from 'node:fs';
+import * as child_process from 'node:child_process';
 
 for (const filename of fs.readdirSync('.')) {
   if (filename.endsWith('.wast') === false) {
@@ -11,4 +12,11 @@ for (const filename of fs.readdirSync('.')) {
     fs.mkdirSync(name);
   }
 
+  // todo, wast2sjon doesnt handle these 2 properly
+  if (name === "comments" || name === "if") {
+    continue;
+  }
+
+  child_process.execSync(`rm -f ${name}/*.*`);
+  child_process.execSync(`wast2json ${filename} -o ${name}/${name}.json`);
 }
