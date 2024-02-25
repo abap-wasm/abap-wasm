3dc33789486752e7f0a9d8b4dcf3a89335e4cc44
Lars Hvam <larshp@hotmail.com>
2024-02-25 09:49:40 +0100

    fix unit

--- test/build_comment.mjs
+++ test/build_comment.mjs
@@ -10,11 +10,11 @@ let comment = "Regression test results:\n";
 
 comment += "|           | Before | After | Delta |\n";
 comment += "| :---      | ---:   | ---:  | ---:  |\n";
-comment += `| :green_circle: Successes | ${before.successes} | ${after.successes} | ${after.successes - before.successes} |\n`;
-comment += `| :yellow_circle: Warnings | ${before.warnings}  | ${after.warnings}  | ${after.warnings - before.warnings} |\n`;
-comment += `| :red_circle: Errors      | ${before.errors}    | ${after.errors}    | ${after.errors - before.errors} |\n`;
-comment += `| :running_man: Runtime    | ${before.runtime}s  | ${after.runtime}s  | ${after.runtime - before.runtime}s |\n`;
-comment += `| :partying_face: QuickJS  | ${qbefore.runtime}s | ${qafter.runtime}s | ${qafter.runtime - qbefore.runtime}ms |\n`;
+comment += `| :green_circle: Successes | ${before.successes}  | ${after.successes}  | ${after.successes - before.successes} |\n`;
+comment += `| :yellow_circle: Warnings | ${before.warnings}   | ${after.warnings}   | ${after.warnings - before.warnings} |\n`;
+comment += `| :red_circle: Errors      | ${before.errors}     | ${after.errors}     | ${after.errors - before.errors} |\n`;
+comment += `| :running_man: Runtime    | ${before.runtime}s   | ${after.runtime}s   | ${after.runtime - before.runtime}s |\n`;
+comment += `| :partying_face: QuickJS  | ${qbefore.runtime}ms | ${qafter.runtime}ms | ${qafter.runtime - qbefore.runtime}ms |\n`;
 
 comment += "\nUpdated: " + new Date().toISOString() + "\n";
 comment += "\nSHA: " + process.env.GITHUB_SHA + "\n";
