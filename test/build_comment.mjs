8a6b86138f6c6d55bf8faf0b76b5c459d240f176
Lars Hvam <larshp@hotmail.com>
2024-02-29 18:13:21 +0100

    update

--- test/build_comment.mjs
+++ test/build_comment.mjs
@@ -13,7 +13,8 @@ let comment = "Regression test results:\n\n";
 comment += "|           | Successes | Warnings | Errors | Runtime |\n";
 comment += "| :---      | ---:      | ---:     | ---:   | ---:    |\n";
 
-let totalSuccesses = 0;
+let totalSuccessesAfter = 0;
+let totalSuccessesBefore = 0;
 for (const folder of folders) {
   const after = JSON.parse(fs.readFileSync(`../after/${folder}.json`, "utf-8"));
   const before = JSON.parse(fs.readFileSync(`../before/${folder}.json`, "utf-8"));
@@ -21,7 +22,8 @@ for (const folder of folders) {
   const good = " :white_check_mark:";
   const bad = " :small_red_triangle_down:"
   let successes = after.successes;
-  totalSuccesses += after.successes;
+  totalSuccessesAfter += after.successes;
+  totalSuccessesBefore += before.successes;
   if (before.successes !== after.successes) {
     let delta = after.successes - before.successes;
     successes += " (" + delta + (delta > 0 ? good : bad ) + ")";
@@ -32,7 +34,12 @@ for (const folder of folders) {
   comment += `| ${folder} | ${successes}  | ${warnings}  | ${errors} | ${runtime}s |\n`;
 }
 
-comment += `Total Succeses: *${totalSuccesses}*\n`;
+comment += `Total Succeses: *${totalSuccessesAfter}*`;
+if (totalSuccessesBefore !== totalSuccessesAfter) {
+  let delta = totalSuccessesAfter - totalSuccessesBefore;
+  comment += " (" + delta + (delta > 0 ? good : bad ) + ")";
+}
+comment += "\n";
 
 comment += "\nUpdated: " + new Date().toISOString() + "\n";
 comment += "\nSHA: " + process.env.GITHUB_SHA + "\n";
