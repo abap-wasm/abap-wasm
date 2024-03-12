fd778bd65b92e1effb35b879ca684ebdece0dc26
Lars Hvam <larshp@hotmail.com>
2024-03-12 19:35:06 +0100

    cleanup

--- test/build_comment.mjs
+++ test/build_comment.mjs
@@ -54,6 +54,7 @@ const scryptAfter = JSON.parse(fs.readFileSync(`../after/scrypt.json`, "utf-8"))
 const scryptBefore = JSON.parse(fs.readFileSync(`../before/scrypt.json`, "utf-8"));
 const sha256After = JSON.parse(fs.readFileSync(`../after/sha256.json`, "utf-8"));
 const sha256Before = JSON.parse(fs.readFileSync(`../before/sha256.json`, "utf-8"));
+comment += "Performance:\n";
 comment += "|           | Before | After | Delta |\n";
 comment += "| :---      | ---:   | ---:  | ---:  |\n";
 comment += `| :partying_face: QuickJS | ${quickjsBefore.runtime}ms | ${quickjsAfter.runtime}ms | ${quickjsAfter.runtime - quickjsBefore.runtime}ms |\n`;
@@ -62,8 +63,6 @@ comment += `| :sunglasses: SHA256 | ${sha256Before.runtime}ms | ${sha256After.ru
 
 const performanceAfter = JSON.parse(fs.readFileSync(`../after/performance.json`, "utf-8"));
 const performanceBefore = JSON.parse(fs.readFileSync(`../before/performance.json`, "utf-8"));
-comment += "|           | Before | After | Delta |\n";
-comment += "| :---      | ---:   | ---:  | ---:  |\n";
 for (const row of performanceAfter) {
   let before = 0;
   for (const bar of performanceBefore) {
