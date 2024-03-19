8ab841b919cf26328dea6f2fa1201ffd7e6615e2
Lars Hvam <larshp@hotmail.com>
2024-03-19 12:39:28 +0100

    something like this

---
+++ test/build-fast.mjs
@@ -0,0 +1,20 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const FOLDER = "dist-fast";
+
+if (fs.existsSync(FOLDER)) {
+  fs.rmSync(FOLDER, { recursive: true });
+}
+fs.mkdirSync(FOLDER);
+fs.mkdirSync(FOLDER + path.sep + "src");
+
+for (const r of fs.readdirSync("src", { withFileTypes: true, recursive: true })) {
+  if (r.isFile()) {
+    let text = fs.readFileSync(r.path + path.sep + r.name).toString("utf-8");
+    text = text.replace(/\s*"##feature-start=debug([\w\W]*?)"##feature-end=debug/, "");
+    fs.writeFileSync(FOLDER + path.sep + r.path + path.sep + r.name, text);
+  } else {
+    fs.mkdirSync(FOLDER + path.sep + r.path + path.sep + r.name);
+  }
+}
